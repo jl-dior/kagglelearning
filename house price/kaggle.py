128623b82daeabd2876fbbd91e5ed47523fe492d
mac <920411048@qq.com>
2020-03-20 17:23:33 +0800

    idea

--- house price/kaggle.py
+++ house price/kaggle.py
@@ -2,8 +2,8 @@ import os
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-# import seaborn as sns 
-# from scipy.stats import mode, skew
+import seaborn as sns
+from scipy.stats import mode, skew
 
 
 house_price = pd.read_csv('train.csv')
